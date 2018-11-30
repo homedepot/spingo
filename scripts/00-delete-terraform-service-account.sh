282704ded44bd7d557edfc2488d746ba5afd6b28
SVC_CICD <jeffrey_k_billimek@homedepot.com>
2018-11-30 14:08:22 -0500

    renaming and creating delete script
    Signed-off-by: SVC_CICD <jeffrey_k_billimek@homedepot.com>

---
+++ scripts/00-delete-terraform-service-account.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# PREREQUISITES
+# logged-in to gcloud: `gcloud auth login --project np-platforms-cd-thd`
+# logged into vault
+# `export VAULT_ADDR=https://vault.ioq1.homedepot.com:10231`
+# `vault login <some token>`
+
+# if you need to delete the service account, read https://cloud.google.com/iam/docs/understanding-service-accounts#deleting_and_recreating_service_accounts
+
+PROJECT=np-platforms-cd-thd
+SERVICE_ACCOUNT_NAME=terraform-account
+SERVICE_ACCOUNT_DEST=terraform-account.json
+
+SA_EMAIL=$(gcloud iam service-accounts list \
+    --filter="displayName:${SERVICE_ACCOUNT_NAME}" \
+    --format='value(email)')
+
+PROJECT=$(gcloud info --format='value(config.project)')
+
+echo "removing compute.admin & iam.serviceAccountUser roles from $SERVICE_ACCOUNT_NAME"
+gcloud --no-user-output-enabled projects remove-iam-policy-binding "$PROJECT" \
+    --member serviceAccount:"$SA_EMAIL" \
+    --role roles/iam.serviceAccountUser
+gcloud --no-user-output-enabled projects remove-iam-policy-binding  "$PROJECT" \
+    --member serviceAccount:"$SA_EMAIL" \
+    --role roles/compute.admin
+
+echo "deleting $SERVICE_ACCOUNT_NAME service account"
+gcloud -q iam service-accounts delete "$SERVICE_ACCOUNT_NAME@$PROJECT.iam.gserviceaccount.com"
+
+echo "deleting secret/$SERVICE_ACCOUNT_DEST from vault"
+vault delete secret/$SERVICE_ACCOUNT_NAME
---
+++ scripts/01-create-terraform-service-account.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# PREREQUISITES
+# logged-in to gcloud: `gcloud auth login --project np-platforms-cd-thd`
+# logged into vault
+# `export VAULT_ADDR=https://vault.ioq1.homedepot.com:10231`
+# `vault login <some token>`
+
+# if you need to delete the service account, see 00-delete-terraform-account.sh
+
+echo "enabling compute.googleapis.com service"
+gcloud services enable compute.googleapis.com
+
+PROJECT=np-platforms-cd-thd
+SERVICE_ACCOUNT_NAME=terraform-account
+SERVICE_ACCOUNT_DEST=terraform-account.json
+
+echo "creating $SERVICE_ACCOUNT_NAME service account"
+gcloud iam service-accounts create \
+    "$SERVICE_ACCOUNT_NAME" \
+    --display-name "$SERVICE_ACCOUNT_NAME"
+
+SA_EMAIL=$(gcloud iam service-accounts list \
+    --filter="displayName:${SERVICE_ACCOUNT_NAME}" \
+    --format='value(email)')
+
+PROJECT=$(gcloud info --format='value(config.project)')
+
+echo "adding compute.admin & iam.serviceAccountUser roles to $SERVICE_ACCOUNT_NAME"
+gcloud --no-user-output-enabled projects add-iam-policy-binding "$PROJECT" \
+    --member serviceAccount:"$SA_EMAIL" \
+    --role='roles/iam.serviceAccountUser'
+gcloud --no-user-output-enabled projects add-iam-policy-binding "$PROJECT" \
+    --member serviceAccount:"$SA_EMAIL" \
+    --role='roles/compute.admin'
+
+echo "generating keys for $SERVICE_ACCOUNT_NAME"
+gcloud iam service-accounts keys create "$SERVICE_ACCOUNT_DEST" \
+    --iam-account "$SA_EMAIL"
+
+echo "writing $SERVICE_ACCOUNT_DEST to vault in secret/terraform-account & deleting temp file"
+vault write secret/terraform-account value=@${SERVICE_ACCOUNT_DEST} && rm "$SERVICE_ACCOUNT_DEST"

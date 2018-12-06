e430891830b546e89161cb1886de30b76aaf478a
Jeff Billimek <jeffrey_k_billimek@homedepot.com.com>
2018-12-06 15:41:32 -0500

    removing horrible commented code

--- modules/spin-k8s-cluster/main.tf
+++ modules/spin-k8s-cluster/main.tf
@@ -1,8 +1,5 @@
 ############################################
 resource "google_container_cluster" "cluster" {
-  # count              = "${length(var.cluster_regions)}"
-  # name               = "${var.cluster_name}-${var.cluster_regions[count.index]}-${count.index}"
-  # region             = "${var.cluster_regions[count.index]}"
   name               = "${var.cluster_name}-${var.cluster_region}"
   region             = "${var.cluster_region}"
   min_master_version = "${var.gke_version}"
@@ -16,13 +13,10 @@ resource "google_container_cluster" "cluster" {
   # https://github.com/terraform-providers/terraform-provider-kubernetes/pull/73
   enable_legacy_abac = "${var.enable_legacy_abac}"
 
-  # master_authorized_networks_config {
-  #   cidr_blocks = ["${var.master_authorized_network_cidrs}"]
-  # }
-
   # Remove the default node pool during cluster creation.
   # We use google_container_node_pools for better control and
   # less disruptive changes.
+  # https://github.com/terraform-providers/terraform-provider-google/issues/1712#issuecomment-410317055
   remove_default_node_pool = true
 
   node_pool {
@@ -38,10 +32,6 @@ resource "google_container_cluster" "cluster" {
 
 # Primary node pool
 resource "google_container_node_pool" "primary_pool" {
-  # count              = "${length(var.cluster_regions)}"
-  # name               = "${var.cluster_name}-${var.cluster_regions[count.index]}-${count.index}-primary-pool"
-  # cluster            = "${google_container_cluster.cluster.*.name[count.index]}"
-  # region             = "${var.cluster_regions[count.index]}"
   name               = "${var.cluster_name}-${var.cluster_region}-primary-pool"
   cluster            = "${google_container_cluster.cluster.name}"
   region             = "${var.cluster_region}"

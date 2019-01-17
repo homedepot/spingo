715befe85976061fee892bdaed96b9a35434573d
Jeff Billimek <jeffrey_k_billimek@homedepot.com.com>
2019-01-17 14:36:26 -0500

    parms for more things

--- scripts/setup_hal_docker.sh
+++ scripts/setup_hal_docker.sh
@@ -2,7 +2,16 @@
 
 # set target directory where the bucket will sync locally and where the local kubeconfig file will live
 # these two things are necessary to volume-mount into the docker container
-export HAL_RUN_DIR=~/tmp
+HAL_RUN_DIR=~/tmp
+
+# google cloud project to use
+PROJECT="np-platforms-cd-thd"
+
+# service account name to use
+SERVICE_ACCOUNT="spinnaker-us-east1"
+
+# bucket to use
+BUCKET="np-platforms-cd-thd-halyard-bucket"
 
 # base64 operates differently in OSX vs linux
 if [[ "$OSTYPE" == "darwin"* ]] && [[ ! -f /usr/local/bin/base64 ]]; then
@@ -33,8 +42,8 @@ else
     fi
     echo "pulling google cloud bucket to local directory: ${HAL_RUN_DIR}/spinnaker"
     echo "after making changes to the hal config, you can 'push' local changes back to the bucket by running"
-    echo -e "gsutil -m rsync -d -r ${HAL_RUN_DIR}/spinnaker gs://np-platforms-cd-thd-halyard-bucket\n"
-    gsutil -m rsync -d -r gs://np-platforms-cd-thd-halyard-bucket "${HAL_RUN_DIR}"/spinnaker
+    echo -e "gsutil -m rsync -d -r ${HAL_RUN_DIR}/spinnaker gs://${BUCKET}\n"
+    gsutil -m rsync -d -r gs://"${BUCKET}" "${HAL_RUN_DIR}"/spinnaker
 
     if [[ "$?" != 0 ]]; then
         echo "something went wrong, not proceeding!"
@@ -45,14 +54,14 @@ else
     if [[ ! -f "${HAL_RUN_DIR}"/.kube_spinnaker/config ]]; then
         echo "spinnaker cluster kubectl config file does not exist, creating it now"
         mkdir -p "${HAL_RUN_DIR}"/.kube_spinnaker
-        gcloud beta container clusters get-credentials spinnaker-us-east1 --region us-east1 --project np-platforms-cd-thd
+        gcloud beta container clusters get-credentials "$SERVICE_ACCOUNT" --region us-east1 --project "$PROJECT"
         kubectl config set-credentials spin_cluster_account --token=$(kubectl get secret $(kubectl get secret --namespace=kube-system | grep default-token | awk '{print $1}') --namespace=kube-system -o jsonpath={.data.token} | base64 ${BASE64_DECODE})
         kubectl config set-context $(kubectl config current-context) --user=spin_cluster_account
     fi
 
     if [ ! "$(docker ps -q -f name=halyard)" ]; then
         if [ "$(docker ps -aq -f status=exited -f name=halyard)" ]; then
-            echo "\nremoving old halyard image"
+            echo -e "\nremoving old halyard image"
             docker rm halyard
         fi
         echo -e "\nstarting docker container named 'halyard'"

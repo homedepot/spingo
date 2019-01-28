6cd09ce871a08baf19741016452483e5e6f0ab02
cgg3eqj <christopher_g_graves@homedepot.com>
2019-01-28 09:00:53 -0500

    added set container needed to reset back.

--- modules/gce/halyard/halScripts/resetgcp.sh
+++ modules/gce/halyard/halScripts/resetgcp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 gcloud auth activate-service-account --key-file="/home/${USER}/${USER}.json" --project "${PROJECT}"
+gcloud config set container/use_client_certificate true
 gcloud beta container clusters get-credentials "${USER}-${REGION}" --region "${REGION}" --project "${PROJECT}"
 kubectl config set-credentials "${SPIN_CLUSTER_ACCOUNT}" --token=$(kubectl get secret $(kubectl get secret --namespace=kube-system | grep default-token | awk '{print $1}') --namespace=kube-system -o jsonpath={.data.token} | base64 -d)
 kubectl config set-context $(kubectl config current-context) --user="${SPIN_CLUSTER_ACCOUNT}"

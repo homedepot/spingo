a749a209b8d1454164bf732629a4713a9b9f62d7
Jeff Billimek <jeffrey_k_billimek@homedepot.com.com>
2019-02-26 11:51:32 -0500

    adding new script to handle onboarding
    This will onboard new remote k8s clusters by creating a new 'spinnaker-user' service account
    with admin access for use by halyard to do activities against the remote cluster

---
+++ scripts/remote-project-onboarding/create-remote-gke-spinnaker-sa-account.sh
@@ -0,0 +1,63 @@
+#!/bin/bash 
+
+# Inspired by: https://stackoverflow.com/questions/42170380/how-to-add-users-to-kubernetes-kubectl
+# this script creates a service account (spinnaker-user) on a Kubernetes cluster (tested with AWS EKS 1.9)
+# prereqs: a kubectl ver 1.10 installed and proper configuration of the heptio authenticator
+# this has been tested on Linux in a Cloud9 environment (for MacOS the syntax may be slightly different)
+
+####################################################
+########           Create an account        ######## 
+####################################################
+# Create service account for user spinnaker-user
+kubectl create sa spinnaker-user
+# Get related secret
+secret=$(kubectl get sa spinnaker-user -o json | jq -r '.secrets[].name')
+# Get ca.crt from secret 
+kubectl get secret $secret -o json | jq -r '.data["ca.crt"]' | base64 -d > ca.crt
+# Get service account token from secret
+user_token=$(kubectl get secret $secret -o json | jq -r '.data["token"]' | base64 -d)
+# Get information from your kubectl config (current-context, server..)
+# get current context
+c=`kubectl config current-context`
+# get cluster name of context
+name=`kubectl config get-contexts $c | awk '{print $3}' | tail -n 1`
+# get endpoint of current context 
+endpoint=`kubectl config view -o jsonpath="{.clusters[?(@.name == \"$name\")].cluster.server}"`
+
+# Create the yaml to bind the cluster admin role to spinnaker-user
+cat <<EOF >> rbac-config-spinnaker-user.yaml
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: spinnaker-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+  - kind: ServiceAccount
+    name: spinnaker-user
+    namespace: default
+EOF
+
+# Apply the policy to spinnaker-user
+## nota bene: this command is running against the GKE admin account (defaulting to a reference in ~/.kube/config)
+kubectl apply -f rbac-config-spinnaker-user.yaml && rm rbac-config-spinnaker-user.yaml
+
+
+####################################################
+########          Consume the account       ######## 
+####################################################
+
+# Set cluster 
+## TODO: need to put (and reference) the "${c}.config" file in a proper path for halyard to reference for realz
+kubectl config set-cluster $c --embed-certs=true --server=$endpoint --certificate-authority=./ca.crt --kubeconfig="${c}.config" && rm ca.crt
+# Set user credentials 
+kubectl config set-credentials "spinnaker-user-${c}" --token=$user_token --kubeconfig="${c}.config"
+
+# Define the combination of spinnaker-user user with the EKS cluster
+kubectl config set-context "spinnaker-user-${c}" --cluster="$c" --user="spinnaker-user-${c}" --namespace=default --kubeconfig="${c}.config"
+kubectl config use-context "spinnaker-user-${c}" --kubeconfig="${c}.config"
+
+echo -e "\n\nprovide the ${c}.config file to the spinnaker admins."
+echo "THIS FILE CONTAINS SECRETS AND SHOULD BE HANDLED AS SUCH"

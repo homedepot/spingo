d3fb45dad3317c8559a567328662533bf7ef8d77
Billimek, Jeffrey K <JEFFREY_K_BILLIMEK@homedepot.com>
2019-03-14 13:19:40 -0400

    adding gsutil cp command to get kubconfig file (#28)

--- scripts/remote-project-onboarding/add-remote-cluster.sh
+++ scripts/remote-project-onboarding/add-remote-cluster.sh
@@ -5,16 +5,19 @@ set -x
 ## variables that will change for each target
 ## TODO: what can we do to automate the gathering of these variables?
 PROJECT="hd-sqa-nonprod"
-REGION="us-central1-a"
-CLUSTER="helloworld--non-legacy-auth"
-KUBE_FILE="/spinnaker/accounts/gke_hd-sqa-nonprod_us-central1-a_helloworld--non-legacy-auth.config"
-CONTEXT="gke_hd-sqa-nonprod_us-central1-a_helloworld--non-legacy-auth"
+REGION="us-east1"
+CLUSTER="spinnaker-us-east1"
+KUBE_FILE="/spinnaker/accounts/gke_np-platforms-cd-thd_us-east1_spinnaker-us-east1.config"
+CONTEXT="gke_np-platforms-cd-thd_us-east1_spinnaker-us-east1"
+GROUP="TBD"
+
+gsutil cp gs://np-platforms-cd-thd-spinnaker-onboarding/gke_np-platforms-cd-thd_us-east1_spinnaker-us-east1.config $KUBE_FILE
 
 hal config provider kubernetes account add "${PROJECT}-${REGION}-${CLUSTER}" \
     --context "$CONTEXT" \
     --provider-version v2 \
     --docker-registries "docker-registry" \
-    --write-permissions=gg_pim_sshblk11_np \
-    --read-permissions=gg_pim_sshblk11_np \
+    --write-permissions="$GROUP" \
+    --read-permissions="$GROUP" \
     --only-spinnaker-managed=true \
     --kubeconfig-file="$KUBE_FILE"

5f15da8d2a61435f3d4ee407085855a2cfeb58af
Nathaniel P Maile <nathaniel_p_maile@homedepot.com>
2019-10-21 12:47:19 -0400

    fixed indenting in if statements, added sanity check, and gave exact command to export vault location

--- scripts/local_vault_setup.sh
+++ scripts/local_vault_setup.sh
@@ -3,16 +3,16 @@ set -e
 vaultdir="$HOME/vault"
 mkdir -p $vaultdir
 if [ ! "$(command -v vault)" ]; then
-echo "downloading Vault for Linux"
-wget -O "$vaultdir/vault.zip" "https://releases.hashicorp.com/vault/1.2.3/vault_1.2.3_linux_amd64.zip"
-unzip "$vaultdir/vault.zip" $vaultdir/vault
-sudo ln -s "$vaultdir/vault" "/usr/bin/vault"
+	echo "downloading Vault for Linux"
+	wget -O "$vaultdir/vault.zip" "https://releases.hashicorp.com/vault/1.2.3/vault_1.2.3_linux_amd64.zip"
+	unzip "$vaultdir/vault.zip" $vaultdir/vault
+	sudo ln -s "$vaultdir/vault" "/usr/bin/vault"
 fi
 
 if [ ! -f $vaultdir/config.hcl ]; then
-echo "generating config.hcl file"
+	echo "generating config.hcl file"
 
-tee $vaultdir/config.hcl << CONFIGURATION
+	tee $vaultdir/config.hcl << CONFIGURATION
 storage "file" {
 	address = "127.0.0.1:8500"
 	path = "$vaultdir/secrets"
@@ -23,6 +23,7 @@ listener "tcp" {
 	tls_disable = 1
 }
 CONFIGURATION
+
 fi
 
 vault server --config "$vaultdir/config.hcl" >/dev/null &
@@ -35,10 +36,10 @@ export VAULT_ADDR="http://127.0.0.1:8200"
 vault operator init -n 1 -t 1 > "$vaultdir/initinfo"
 cat $vaultdir/initinfo | sed -n -e 's/Unseal Key 1: \(.*\)$/\1/p' | xargs vault operator unseal
 cat $vaultdir/initinfo | sed -n -e 's/Initial Root Token: \(.*\)$/\1/p' | vault login -
-
+vault status
 echo "vault should be set up now"
 
 if [ -n $VAULT_ADDR_NOT_SET ]; then
-	echo "please set \$VAULT_ADDR to \"http://127.0.0.1:8200\""
+	echo -e "please set \$VAULT_ADDR to \"http://127.0.0.1:8200\" by running the command on the next line\nexport VAULT_ADDR=\"http://127.0.0.1:8200\"\n"
 fi
 

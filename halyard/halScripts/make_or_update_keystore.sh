0636bd6a34fcfc92a7e4626206258ed9d4e099ff
Chris Gruel <chris_gruel@homedepot.com>
2019-10-22 16:09:32 -0400

    automate certbot through lego

--- halyard/halScripts/make_or_update_keystore.sh
+++ halyard/halScripts/make_or_update_keystore.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-if [ -f "/certstore/certificates/_.${DNS}.json" ] 
-then
+CERBOT_PATH="/${USER}/certbot/"
+CERTSTORE_PATH="/${USER}/certstore/certificates/"
+
+if [ -f "$CERTSTORE_PATH/_.${DNS}.json" ];then
     echo "===== OPERATION: RENEW ====="
     LEGO_CMD="renew --days 45 "
 else
     echo "===== OPERATION: INITIAL REQUEST ====="
     LEGO_CMD="run"
 fi
-
-CERBOT_PATH="/${USER}/certbot/"
-CERTSTORE_PATH="/${USER}/certstore/certificates/"
 
 docker run \
   -e GCE_PROJECT="${PROJECT}" \
@@ -27,9 +26,9 @@ docker run \
       --domains="*.${DNS}" \
       $LEGO_CMD
 
-openssl pkcs12 -export -out "$CERTSTORE_PATH"/wildcard.pkcs12 -in "$CERTSTORE_PATH"/_. ${DNS}.pem -name spinnaker -password pass:"nosecrets"
-keytool -v -importkeystore -srckeystore "$CERTSTORE_PATH"/wildcard.pkcs12 -destkeystore "$CERTSTORE_PATH"/wildcard.jks -deststoretype JKS -storepass "nosecrets" -srcstorepass "nosecrets" -noprompt
-keytool -trustcacerts -keystore "$CERTSTORE_PATH"/wildcard.jks -importcert -file "$CERTSTORE_PATH"/_. ${DNS}.crt -storepass "nosecrets" -noprompt # this will fail if certificate renewal instead of new cert but that is ok
+openssl pkcs12 -export -out "$CERTSTORE_PATH"/wildcard.pkcs12 -in "$CERTSTORE_PATH"/_.${DNS}.pem -name spinnaker -password pass:"${KEYSTORE_PASS}"
+keytool -v -importkeystore -srckeystore "$CERTSTORE_PATH"/wildcard.pkcs12 -destkeystore "$CERTSTORE_PATH"/wildcard.jks -deststoretype JKS -storepass "${KEYSTORE_PASS}" -srcstorepass "${KEYSTORE_PASS}" -noprompt
+keytool -trustcacerts -keystore "$CERTSTORE_PATH"/wildcard.jks -importcert -file "$CERTSTORE_PATH"/_.${DNS}.crt -storepass "${KEYSTORE_PASS}" -noprompt # this will fail if certificate renewal instead of new cert but that is ok
 
 cp "$CERTSTORE_PATH"/_.${DNS}.key "$CERBOT_PATH"/${DNS}_wildcard.key
 cp "$CERTSTORE_PATH"/_.${DNS}.crt "$CERBOT_PATH"/${DNS}_wildcard.crt

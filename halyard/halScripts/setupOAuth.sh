2873c66e812fe010ee2c6505b13826cb59b694ce
Chris Gruel <chris_gruel@homedepot.com>
2019-10-23 09:44:52 -0400

    OAuth docs (#41)

--- halyard/halScripts/setupOAuth.sh
+++ halyard/halScripts/setupOAuth.sh
@@ -26,3 +26,6 @@ hal config security authz google edit \
 hal config security authz edit --type google
    
 hal config security authz enable
+
+echo "Running Spinnaker OAuth deployment for deployment named ${DEPLOYMENT_NAME}"
+hal deploy apply

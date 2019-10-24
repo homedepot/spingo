c3c213ab27ba8c9c6baff128fa54f99a7c256cc6
npmaile <nathaniel_p_maile@homedepot.com>
2019-10-24 10:40:46 -0400

    seems to do what it's name is

--- scripts/add_current_ip_to_cluster_allowed.sh
+++ scripts/add_current_ip_to_cluster_allowed.sh
@@ -1,15 +1,52 @@
 #!/usr/bin/env bash
-function addIps(INSTANCE){
-  CLUSTERNAME=$INSTANCE-$REGION
-  list=gcloud container clusters describe $CLUSTERNAME | jq '.masterAuthorizedNetworksConfig.cidrBlocks[].cidrBlock' --region $REGION
-  newlist=''
-  for line in $list; do
-    newlist = $newlist,$line
-  done
-  newlist=$newlist,$(curl ifconfig.co)/32
-  gcloud container clusters update $CLUSTERNAME --enable-master-authorized-networks --master-authorized-networks $newlist --region $REGION
+set -e
+#I(N8) have no idea what the program does. I take no responsibility for it.
+exists() {
+    list=$1[@]
+    name="$2"
+    RESULT="false"
+    arr=("${!list}")
+    for item in ${arr[@]}
+    do
+        if [ "$item" == "$name" ]; then
+            RESULT="true"
+            break;
+        fi
+    done
+    echo "$RESULT"
 }
+selected_channels=()
+PS3="-----------------------------------------------------------------------------"$'\n'"Enter the number for the Kubernetes Cluster to Add this machine's Ip to (Enter the number for Finished when done) : ";
+select channel in $(gcloud container clusters list --format="value(name)") Finished Cancel
+do
+    if [[ $channel == "" ]]; then
+        echo "You must choose a cluster"
+    elif [ "$channel" == "Finished" ]; then
+        echo "Excellent selections!"
+        break;
+    elif [ "$channel" == "Cancel" ]; then
+        echo "Cancelling at user request"
+        exit 1
+    else
+        do_exist=$(exists selected_channels "$channel")
+        if [[ "$do_exist" == "true" ]] ; then
+            echo "cluster already selected"
+        else
+            selected_channels+=($channel)
+            echo "adding channel $channel to selected channels"
+        fi
+    fi
+done
 
-for instance in 'spinnaker sandbox'; do
-  addIps($instance)
+for cluster in ${selected_channels[@]}
+do
+  location=$(gcloud beta container clusters list --filter="name:$cluster" --format="value(Location)")
+  for cidr in $(gcloud container clusters describe $cluster --region $location --format="json" | jq '.masterAuthorizedNetworksConfig.cidrBlocks[].cidrBlock'); do
+    cidrlist=$cidrlist,$cidr
+  done
+  cidrlist=$(curl -s ifconfig.co)/32$cidrlist
+  cidrlist=$(echo $cidrlist | sed s/\"//g)
+  gcloud container clusters update $cluster --enable-master-authorized-networks --master-authorized-networks $cidrlist --region $location
+  #the next line clears the variable before the next loop. don't touch it!
+  cidrlist= 
 done

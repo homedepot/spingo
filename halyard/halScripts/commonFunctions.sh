58c4a4bae84713d3f804db85fa3f6f80c0f211c8
Chris Gruel <chris_gruel@homedepot.com>
2019-11-07 15:58:18 -0500

    sigh, work with the actual file location

--- halyard/halScripts/commonFunctions.sh
+++ halyard/halScripts/commonFunctions.sh
@@ -2,16 +2,16 @@
 
 update_spin(){
     if [ -d /${USER}/x509 ]; then
-        if [ -L /home/${USER}/.spin/config ]; then
-            unlink /home/${USER}/.spin/config
+        if [ -L /${USER}/.spin/config ]; then
+            unlink /${USER}/.spin/config
         fi
-        ln -s /home/${USER}/.spin/"$1".config /home/${USER}/.spin/config
+        ln -s /${USER}/.spin/"$1".config /${USER}/.spin/config
     fi
 }
 
 update_kube() {
-    if [ -L /home/${USER}/.kube/config ]; then
-        unlink /home/${USER}/.kube/config
+    if [ -L /${USER}/.kube/config ]; then
+        unlink /${USER}/.kube/config
     fi
-    ln -s /home/${USER}/.kube/"$1".config /home/${USER}/.kube/config
+    ln -s /${USER}/.kube/"$1".config /${USER}/.kube/config
 }

9169f6b54552acbf08a122400396418fb5655193
Chris Gruel <chris_gruel@homedepot.com>
2019-12-10 14:23:40 -0500

    create vault dns, kms ring/keys,bucket

---
+++ spinnaker/modules/vault/vars.tf
@@ -0,0 +1,23 @@
+variable "gcp_project" {
+  type        = string
+  description = "GCP project name"
+}
+
+variable "kms_key_ring_self_link" {
+  type        = string
+  description = "The self link of the created KeyRing in the format projects/[project}/locations/{location}/keyRings/{name}"
+}
+
+variable "cluster_key_map" {
+  type        = map(string)
+  description = "The keys of this map will dictate the names of the resources to be created and should be the cluster names"
+}
+
+variable "kms_keyring_name" {
+  type        = string
+  description = "The name of the Cloud KMS KeyRing for asset encryption."
+}
+
+variable "vault_ips_map" {
+  type = map(string)
+}

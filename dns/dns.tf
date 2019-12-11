1dcb3bde320a8d90ab1472edb547586635de9810
Chris Gruel <chris_gruel@homedepot.com>
2019-12-11 17:24:25 -0500

    remove backend default

--- dns/dns.tf
+++ dns/dns.tf
@@ -1,7 +1,3 @@
-terraform {
-  backend "gcs" {
-  }
-}
 
 provider "vault" {
 }
--- halyard/halyard.tf
+++ halyard/halyard.tf
@@ -1,7 +1,4 @@
-terraform {
-  backend "gcs" {
-  }
-}
+
 
 variable "gcp_project" {
   description = "GCP project name"

b34c74de02c905701f88f4d9cb44222b5d79f175
Chris Gruel <chris_gruel@homedepot.com>
2020-01-01 15:42:46 -0500

    fix for SC1090

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -35,7 +35,7 @@ CWD=$(pwd)
 GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
 cd "$GIT_ROOT_DIR" || { echo "failed to change directory to $GIT_ROOT_DIR exiting"; exit 1; }
 
-# shellcheck disable=SC1091
+# shellcheck source=scripts/common.sh
 source "$GIT_ROOT_DIR"/scripts/common.sh
 
 terraform_override() {

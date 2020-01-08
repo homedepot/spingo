54597a3b829a052356e48e034ef3c411429a09f7
Chris Gruel <chris_gruel@homedepot.com>
2020-01-08 10:25:54 -0500

    enabled local cred file option (#74)
    * enabled local cred file option
    
    * enable dynamic dns service account pull
    
    * seperate dns from remote state

--- static_ips/static_ips.tf
+++ static_ips/static_ips.tf
@@ -6,10 +6,8 @@ data "vault_generic_secret" "terraform_account" {
 }
 
 provider "google" {
-  credentials = data.vault_generic_secret.terraform_account.data[var.gcp_project]
-
-  # credentials = file("${var.terraform_account}.json") //! swtich to this if you need to import stuff from GCP
-  project = var.gcp_project
+  credentials = var.use_local_credential_file ? file("${var.terraform_account}.json") : data.vault_generic_secret.terraform_account.data[var.gcp_project]
+  project     = var.gcp_project
 }
 
 resource "google_compute_address" "ui" {

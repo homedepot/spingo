048920265f45125384226af3b132cae358cdeaf7
Billy Bolton <billy@bolton.email>
2020-01-27 09:25:47 -0500

    rename static-ips directory

---
+++ static-ips/static-ips.tf
@@ -0,0 +1,86 @@
+provider "vault" {
+}
+
+data "vault_generic_secret" "terraform_account" {
+  path = "secret/${var.gcp_project}/${var.terraform_account}"
+}
+
+provider "google" {
+  credentials = var.use_local_credential_file ? file("${var.terraform_account}.json") : data.vault_generic_secret.terraform_account.data[var.gcp_project]
+  project     = var.gcp_project
+}
+
+resource "google_compute_address" "ui" {
+  for_each = var.ship_plans
+  name     = "ui-${each.key}"
+  region   = each.value["clusterRegion"]
+}
+
+resource "google_compute_address" "api" {
+  for_each = var.ship_plans
+  name     = "api-${each.key}"
+  region   = each.value["clusterRegion"]
+}
+
+resource "google_compute_address" "api_x509" {
+  for_each = var.ship_plans
+  name     = "api-x509-${each.key}"
+  region   = each.value["clusterRegion"]
+}
+
+resource "google_compute_address" "vault" {
+  for_each = var.ship_plans
+  name     = "vault-${each.key}"
+  region   = each.value["clusterRegion"]
+}
+
+resource "google_compute_address" "cloudnat" {
+  for_each = var.ship_plans
+  name     = "nat-${each.key}"
+  region   = each.value["clusterRegion"]
+  lifecycle {
+    ignore_changes = [users]
+  }
+}
+
+# The static IP address for Halyard is being provisioned here so that the Halyard VM can be destroyed without loosing the IP which has to be added to k8s master whitelist
+resource "google_compute_address" "halyard" {
+  name   = "halyard-external-ip" # needs to be dashes to satisfy regex within provider
+  region = var.region
+}
+
+output "ui_ips_map" {
+  value = { for k, v in var.ship_plans : k => google_compute_address.ui[k].address }
+}
+
+output "api_ips_map" {
+  value = { for k, v in var.ship_plans : k => google_compute_address.api[k].address }
+}
+
+output "api_x509_ips_map" {
+  value = { for k, v in var.ship_plans : k => google_compute_address.api_x509[k].address }
+}
+
+output "vault_ips_map" {
+  value = { for k, v in var.ship_plans : k => google_compute_address.vault[k].address }
+}
+
+output "cloudnat_ips_map" {
+  value = { for k, v in var.ship_plans : k => google_compute_address.cloudnat[k].address }
+}
+
+output "cloudnat_ips" {
+  value = [for k, v in var.ship_plans : google_compute_address.cloudnat[k].address]
+}
+
+output "cloudnat_name_map" {
+  value = { for k, v in var.ship_plans : k => google_compute_address.cloudnat[k].name }
+}
+
+output "halyard_ip" {
+  value = google_compute_address.halyard.address
+}
+
+output "ship_plans" {
+  value = var.ship_plans
+}
---
+++ static-ips/vars.tf
@@ -0,0 +1,28 @@
+######################################################################################
+# Required parameters
+######################################################################################
+
+variable "terraform_account" {
+  type    = string
+  default = "terraform-account"
+}
+
+variable "gcp_project" {
+  type        = string
+  description = "GCP project name"
+}
+
+variable "region" {
+  type        = string
+  description = "the region where the IPs are created - this must be the same region as the rest of the spinnaker cluster configuration"
+}
+
+variable "ship_plans" {
+  type        = map(map(string))
+  description = "The object that describes all of the clusters that need to be built by Spingo"
+}
+
+variable "use_local_credential_file" {
+  type    = bool
+  default = false
+}

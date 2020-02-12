6fbc8804ec745d2360b48fd6cf48daa9131e8c02
Nathaniel P Maile <nathaniel_p_maile@homedepot.com>
2020-02-12 11:09:04 -0500

    fmt

--- spinnaker/modules/metrics/metrics.tf
+++ spinnaker/modules/metrics/metrics.tf
@@ -3,8 +3,8 @@ data "vault_generic_secret" "gcp-oauth" {
 }
 
 resource "random_password" "password" {
-  length = 16
-  special = true
+  length           = 16
+  special          = true
   override_special = "_%@"
 }
 

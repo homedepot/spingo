31997466f056c99c098610c0064c8ee0584a7b6e
npmaile <nathaniel_p_maile@homedepot.com>
2020-02-13 14:42:38 -0500

    ...

--- halyard/hal-scripts/setup-cleanup-operator.sh
+++ halyard/hal-scripts/setup-cleanup-operator.sh
@@ -1,15 +1,2 @@
-#!/usr/bin/env bash
-
-%{ for deployment, details in deployments ~}
-
-need "kubectl"
-
-
-kubectl --kubeconfig="${details.kubeConfig}" create -f https://raw.githubusercontent.com/homedepot/kube-cleanup-operator/master/deploy/rbac.yaml
-kubectl --kubeconfig="${details.kubeConfig}" create -f https://raw.githubusercontent.com/homedepot/kube-cleanup-operator/master/deploy/deployment.yaml
-kubectl --kubeconfig="${details.kubeConfig}" logs -f $(kubectl get pods --namespace spinnaker -l "run=cleanup-operator" -o jsonpath="{.items[0].metadata.name}")
-
-
-echo "Cleanup Operator created on cluster"
-
-%{ endfor ~}
+#!/bin/bash
+${SETUP_CLEANUP_OPERATOR_CONTENTS}

2c6ba127ba47b2c83d19d89cbab19df73669a7c6
Billy Bolton <billy@bolton.email>
2020-03-23 19:21:08 -0400

    update vault agent yml files map in halyard tf

--- halyard/halyard.tf
+++ halyard/halyard.tf
@@ -82,7 +82,7 @@ data "template_file" "vault_agent" {
     USER = var.service_account_name
     SETUP_AGENT_VAULT_CONTENTS = templatefile("./scripts/setup-agent-vault-contents.sh", {
       deployments = { for k, v in data.terraform_remote_state.static_ips.outputs.ship_plans : k => {
-        vaultYaml           = data.terraform_remote_state.spinnaker.outputs.vault_yml_files_map[k]
+        vaultYaml           = data.terraform_remote_state.spinnaker.outputs.vault_agent_yml_files_map[k]
         clusterName         = "${k}"
         kubeConfig          = "/${var.service_account_name}/.kube/${k}-agent.config"
         }

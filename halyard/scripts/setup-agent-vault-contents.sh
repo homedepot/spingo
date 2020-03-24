92ad99964c611997e13b81f59b926db330b70c1b
Billy Bolton <billy@bolton.email>
2020-03-23 20:28:30 -0400

    reference the correct label when listing vault pods

--- halyard/scripts/setup-agent-vault-contents.sh
+++ halyard/scripts/setup-agent-vault-contents.sh
@@ -8,9 +8,9 @@ wget -O /tmp/vault-helm-v0.4.0.tar.gz https://github.com/hashicorp/vault-helm/ar
 
 %{ for deployment, details in deployments ~}
 
-echo "${details.vaultYaml}" | base64 -d > /${USER}/vault/vault_agent_${details.clusterName}_helm_values.yml
+echo "${details.vaultYaml}" | base64 -d > /${USER}/vault/vault_${details.clusterName}-agent_helm_values.yml
 
-echo "Creating vault namespace for deployment ${deployment} in agent cluster"
+echo "Creating vault namespace for deployment ${deployment}-agent"
 
 kubectl --kubeconfig="${details.kubeConfig}" create namespace vault
 
@@ -18,31 +18,31 @@ helm install \
     vault \
     --namespace vault \
     --kubeconfig "${details.kubeConfig}" \
-    --values /${USER}/vault/vault_agent_${details.clusterName}_helm_values.yml \
+    --values /${USER}/vault/vault_${details.clusterName}-agent_helm_values.yml \
     /tmp/vault-helm-v0.4.0.tar.gz
 
-echo "Waiting for vault to be installed for deployment ${deployment} in agent cluster"
+echo "Waiting for vault to be installed for deployment ${deployment}-agent"
 n=0
 until [ $n -ge 20 ]
 do
-  kubectl -n vault get po -l=app.kubernetes.io/name=vault \
+  kubectl -n vault get po -l=app.kubernetes.io/name=vault-agent-injector \
   --kubeconfig="${details.kubeConfig}" \
   -o=jsonpath='{.items[*].status.containerStatuses[*].ready}' | grep -v "false" && break
    n=$((n+1))
-   echo "Vault is not yet installed for deployment ${deployment} waiting..."
+   echo "Vault is not yet installed for deployment ${deployment}-agent waiting..."
    sleep 6
 done
 
-echo "Checking to see if vault was setup successfully for deployment ${deployment} in agent cluster"
+echo "Checking to see if vault was setup successfully for deployment ${deployment}-agent"
 
-kubectl -n vault get po -l=app.kubernetes.io/name=vault \
+kubectl -n vault get po -l=app.kubernetes.io/name=vault-agent-injector \
   --kubeconfig="${details.kubeConfig}" \
   -o=jsonpath='{.items[*].status.containerStatuses[*].ready}' | grep -v "false"
 
 if [ $? -ne 0 ]; then
-    echo "Vault setup for cluster ${deployment} has failed for agent cluster."
+    echo "Vault setup for cluster ${deployment}-agent has failed"
     exit 1
 fi
-echo "Vault is successfully running for cluster ${deployment} in agent cluster"
+echo "Vault is successfully running for cluster ${deployment}-agent"
 
 %{ endfor ~}
